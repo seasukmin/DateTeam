cf94072694a42109b33b2a0d03373d145f20e776
sooyeon00 <iwn0180@naver.com>
2024-06-17 17:34:08 +0900

    6.17.17:34

--- 박근모 - 메인/Javascript/mainPage.js
+++ 박근모 - 메인/Javascript/mainPage.js
@@ -1,4 +1,42 @@
-const jmtTag = document.querySelector(".h-Tag1Span");
-const cafeTag = document.querySelector(".h-Tag2Span");
-const placeTag = document.querySelector(".h-Tag3Span");
-const epliogueTag = document.querySelector(".h-Tag4Span");
+import {
+  db,
+  getDatas,
+  addDatas,
+  deleteDatas,
+  updateDatas,
+} from "../Javascript/DLU_firebase.js";
+// const jmtTag = document.querySelector(".h-Tag1Span");
+// const cafeTag = document.querySelector(".h-Tag2Span");
+// const placeTag = document.querySelector(".h-Tag3Span");
+// const epliogueTag = document.querySelector(".h-Tag4Span");
+
+async function getcutomerInfo() {
+  const datas = await getDatas("customer");
+  datas.forEach((doc) => {
+    // console.log(doc.id);
+    const info = doc.data();
+    // console.log(info.age);
+    let { addr, age, email, gender, tel } = doc.data();
+    console.log(age);
+    console.log(gender);
+  });
+}
+console.log(getcutomerInfo());
+
+// async function getcutomerAge() {
+//   const datas = await getDatas("customer");
+//   datas.forEach((doc) => {
+//     const info = doc.data();
+//     return info.age;
+//   });
+// }
+// console.log(getcutomerAge());
+
+// async function getcutomerGender() {
+//   const datas = await getDatas("customer");
+//   datas.forEach((doc) => {
+//     const info = doc.data();
+//     return info.gender;
+//   });
+// }
+// console.log(getcutomerGender());
