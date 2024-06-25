eade741d1b8d82b51c0470c18fde0e42505b8830
seasukmin <167054580+seasukmin@users.noreply.github.com>
2024-06-25 15:44:00 +0900

    1

---
+++ 서석민 - 맛집/HTML1/DLU_firebase.js
@@ -0,0 +1,83 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import {
+  getFirestore,
+  getDocs,
+  collection,
+  setDoc,
+  doc,
+  addDoc,
+  deleteDoc,
+  getDoc,
+  updateDoc,
+  where,
+  query,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBiyG3AdHa7uWa4miLcAqCQGqQhaC-amD4",
+  authDomain: "daejeon-loveu.firebaseapp.com",
+  projectId: "daejeon-loveu",
+  storageBucket: "daejeon-loveu.appspot.com",
+  messagingSenderId: "952789620772",
+  appId: "1:952789620772:web:8e1b46f0e4c99dab9ea0b1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function getDatas(collectionName) {
+  const collect = await collection(db, collectionName);
+  const snapshot = await getDocs(collect);
+
+  return snapshot;
+}
+
+// query(where()) -> 다른 컬렉션에 객체값을 연결시켜주는 조건!
+async function getUserPlans(collectionName, date, userId) {
+  const collect = await collection(db, collectionName);
+  const q = await query(
+    collect,
+    where("userId", "==", userId),
+    where("date", "==", date)
+  );
+  const querySnapshot = await getDocs(q);
+  return querySnapshot;
+}
+
+async function addDatas(collectionName, dataObj, userName) {
+  try {
+    //   문서 ID 수동
+    // const saveDoc = doc(db, 컬렉션명, 문서ID)
+    // console.log(`doc() 결과 : ${saveDoc}`);
+    // console.log(`setDoc() 결과 : ${saveResult}`);
+    const saveDoc = await doc(db, collectionName, userName);
+    const saveResult = await setDoc(saveDoc, dataObj);
+
+    // 문서 ID 자동
+    // const collect = await collection(db, collectionName);
+    // await addDoc(collect, dataObj);
+
+    return true;
+  } catch (error) {
+    return false;
+  } finally {
+  }
+}
+
+async function deleteDatas(collectionName, docId) {
+  const docRef = await doc(db, collectionName, docId);
+  // deleteDoc(삭제할 문서);
+  await deleteDoc(docRef);
+}
+
+async function updateDatas(collectionName, docId, updateInfoObj) {
+  // doc(db, 컬렉션명, 문서ID);
+  const docRef = await doc(db, collectionName, docId);
+  // getDocs(문서레퍼런스);
+  // const docData = await getDocs(docRef);
+  // updateDoc(문서데이터, 수정할정보);
+  updateDoc(docRef, updateInfoObj);
+}
+
+export { db, getDatas, addDatas, deleteDatas, updateDatas, getUserPlans };
