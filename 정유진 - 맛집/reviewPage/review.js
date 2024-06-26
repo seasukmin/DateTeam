c6531f543f0e1df15182bd3949ddf641b0869f25
sooyeon00 <iwn0180@naver.com>
2024-06-26 15:51:54 +0900

    6.26.15:51

--- 정유진 - 맛집/reviewPage/review.js
+++ 정유진 - 맛집/reviewPage/review.js
@@ -22,78 +22,87 @@ function submitReview() {
   const cafeBox = document.getElementById("cafe-content-box");
   const hotplaceBox = document.getElementById("hp-content-box");
 
-  const category = document.getElementById("category").value;
   // 해당 value에 맞는 div에 추가하기
 
   uploadBtn.addEventListener("click", () => {
+    const category = document.getElementById("category").value;
     const location = document.getElementById("location").value;
     const date = document.getElementById("date").value;
     const photo = document.getElementById("photo").files[0];
     const rating = document.getElementById("rating").value;
     const review = document.getElementById("review").value;
+    function submitReview(place) {
+      if (photo) {
+        //   const storageRef = storage.ref();
+        //   const photoRef = storageRef.child(`photos/${photo.name}`);
+        //   photoRef.put(photo);
+        //   const photoURL = photoRef.getDownloadURL();
 
-    if (photo) {
-      //   const storageRef = storage.ref();
-      //   const photoRef = storageRef.child(`photos/${photo.name}`);
-      //   photoRef.put(photo);
-      //   const photoURL = photoRef.getDownloadURL();
+        const reader = new FileReader();
+        reader.onload = function (event) {
+          const photoURL = event.target.result;
+          const reviewHTML = `
+                                   <div class="tasty-content" id="tasty-box">
+                                       <div class="review-user">
+                                           <i class="bx bxs-user-circle"></i>
+                                           <p class="username">${userId}님의 후기</p>
+                                       </div>
+                                       <div class="review-visited">
+                                           <h2>방문한 곳: ${location}</h2>
+                                           <p>작성일: ${date}</p>
+                                       </div>
+                                       <div class="review-text">
+                                       //   <img src="${photoURL}" alt="Photo" />
+                                       <p>${review}</p>
+                                       </div>
+                                       <div class="review-point">
+                                           <p>평점: ${rating}</p>
+                                           <div class="keyword">
+                                           <p>친절해요</p>
+                                           <p>청결해요</p>
+                                               <p>또 가고싶어요</p>
+                                           </div>
+                                           </div>
+                                           </div>`;
 
-      const reader = new FileReader();
-      reader.onload = function (event) {
-        const photoURL = event.target.result;
+          place.insertAdjacentHTML("beforeend", reviewHTML);
+          hiddenForm.classList.add("hiddenForm");
+        };
+      } else {
         const reviewHTML = `
-                          <div class="tasty-content" id="tasty-box">
-                              <div class="review-user">
-                                  <i class="bx bxs-user-circle"></i>
-                                  <p class="username">${userId}님의 후기</p>
-                              </div>
-                              <div class="review-visited">
-                                  <h2>방문한 곳: ${location}</h2>
-                                  <p>작성일: ${date}</p>
-                              </div>
-                              <div class="review-text">
-                              //   <img src="${photoURL}" alt="Photo" />
-                              <p>${review}</p>
-                              </div>
-                              <div class="review-point">
-                                  <p>평점: ${rating}</p>
-                                  <div class="keyword">
-                                  <p>친절해요</p>
-                                  <p>청결해요</p>
-                                      <p>또 가고싶어요</p>
-                                  </div>
-                                  </div>
-                                  </div>`;
+                             <div class="tasty-content" id="tasty-box">
+                                 <div class="review-user">
+                                     <i class="bx bxs-user-circle"></i>
+                                     <p class="username">${userId}님의 후기</p>
+                                 </div>
+                                 <div class="review-visited">
+                                     <h2>방문한 곳: ${location}</h2>
+                                     <p>작성일: ${date}</p>
+                                 </div>
+                                 <div class="review-text">
+                                     <p>${review}</p>
+                                 </div>
+                                 <div class="review-point">
+                                     <p>평점: ${rating}</p>
+                                     <div class="keyword">
+                                         <p>친절해요</p>
+                                         <p>청결해요</p>
+                                         <p>또 가고싶어요</p>
+                                     </div>
+                                 </div>
+                             </div>`;
 
-        tastyBox.insertAdjacentHTML("beforeend", reviewHTML);
+        place.insertAdjacentHTML("beforeend", reviewHTML);
         hiddenForm.classList.add("hiddenForm");
-      };
+      }
+    }
+    if (category == "tasty") {
+      submitReview(tastyBox);
+    } else if (category == "cafe") {
+      submitReview(cafeBox);
+    } else if (category == "hotPlace") {
+      submitReview(hotplaceBox);
     } else {
-      const reviewHTML = `
-                    <div class="tasty-content" id="tasty-box">
-                        <div class="review-user">
-                            <i class="bx bxs-user-circle"></i>
-                            <p class="username">${userId}님의 후기</p>
-                        </div>
-                        <div class="review-visited">
-                            <h2>방문한 곳: ${location}</h2>
-                            <p>작성일: ${date}</p>
-                        </div>
-                        <div class="review-text">
-                            <p>${review}</p>
-                        </div>
-                        <div class="review-point">
-                            <p>평점: ${rating}</p>
-                            <div class="keyword">
-                                <p>친절해요</p>
-                                <p>청결해요</p>
-                                <p>또 가고싶어요</p>
-                            </div>
-                        </div>
-                    </div>`;
-
-      tastyBox.insertAdjacentHTML("beforeend", reviewHTML);
-      hiddenForm.classList.add("hiddenForm");
     }
   });
 }
